23668dfe9ea7a2e38c99d1e6b8f92685ce8c8904
cqb13 <cqb13.main@gmail.com>
2025-05-21 13:50:34 -0400

    better addon display & verified filter

--- src/app.tsx
+++ src/app.tsx
@@ -4,18 +4,71 @@ import type { Addon } from "./helpers/addon";
 
 export function App() {
   const [addons, setAddons] = useState<Addon[]>([]);
+  const [visibleAddons, setVisibleAddons] = useState<Addon[]>([]);
+  const [totalAddons, setTotalAddons] = useState<number>(0);
+
+  // filters
+  const [verifiedOnly, setVerifiedOnly] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
       const addons = await loadAddons();
+      setTotalAddons(addons.length);
       setAddons(addons);
     })();
   }, []);
 
+  useEffect(() => {
+    updateVisibleAddons();
+  }, [addons, verifiedOnly]);
+
+  function updateVisibleAddons() {
+    let visible: Addon[] = [];
+
+    addons.forEach((addon: Addon) => {
+      if ((verifiedOnly && addon.verified) || !verifiedOnly) {
+        visible.push(addon);
+      }
+    });
+    setVisibleAddons(visible);
+  }
+
   return (
     <>
       <header></header>
-      <main></main>
+      <main class="flex flex-col gap-2 justify-center items-center p-5">
+        <section class="flex justify-between w-11/12">
+          <div class="flex gap-1 justify-center items-center select-none">
+            <div
+              class={`border rounded w-5 h-5 cursor-pointer hover:bg-purple-400 transition-all duration-300 ease-in-out active:bg-purple-400/50 ${verifiedOnly ? "bg-purple-500" : null}`}
+              onClick={() => setVerifiedOnly(!verifiedOnly)}
+            />
+            <p>Verified Only</p>
+          </div>
+          <div>
+            <p>
+              {visibleAddons.length}/{totalAddons}
+            </p>
+          </div>
+        </section>
+        <section class="flex gap-2 flex-wrap justify-center items-center">
+          {visibleAddons?.map((addon: Addon, key: number) => (
+            <div key={key} class="w-96 h-52 block border rounded p-2">
+              <div class="flex gap-1">
+                {addon.icon ? (
+                  <img src={addon.icon} alt="icon" class="w-16 h-16 rounded" />
+                ) : null}
+                <div>
+                  <p>{addon.name}</p>
+                  <p>{addon.repo.owner}</p>
+                </div>
+              </div>
+              <p>{addon.summary}</p>
+              <div></div>
+            </div>
+          ))}
+        </section>
+      </main>
       <footer></footer>
     </>
   );
