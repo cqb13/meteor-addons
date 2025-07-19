41aff03afe8defc1068a3bed6030076ca865a4d3
cqb13 <cqb13.main@gmail.com>
2025-07-19 18:47:30 -0400

    actually read addons.json

--- src/helpers/addonLoader.ts
+++ src/helpers/addonLoader.ts
@@ -1,12 +1,9 @@
 import type Addon from "./addon";
 
 export default async function loadAddons(): Promise<Addon[]> {
-  // const res = await fetch(
-  //   `https://raw.githubusercontent.com/cqb13/meteor-addon-scanner/refs/heads/main/addons.json?v=${new Date().getTime()}`,
-  // );
-  //
-
-  const res = await fetch("/assets/addons.json");
+  const res = await fetch(
+    `https://raw.githubusercontent.com/cqb13/meteor-addon-scanner/refs/heads/main/addons.json?v=${new Date().getTime()}`,
+  );
 
   const addons: Addon[] = await res.json();
 
