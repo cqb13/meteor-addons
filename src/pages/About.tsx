fb310ae3fec6920ec60587f0a0e51ab142663932
cqb13 <cqb13.main@gmail.com>
2025-08-16 18:47:47 -0400

    added custom data section with json template

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,7 +15,7 @@ const About: FunctionalComponent<RoutableProps> = () => {
         <h2 class="font-medium text-4xl pb-2">Developer Info</h2>
         <article>
           <h3 class="text-xl">How to Verify Your Addon</h3>
-          <article class="text-slate-400">
+          <article class="text-slate-400 flex flex-col gap-2">
             <p>
               To verify your addon you must join the discord server and create a
               post in the{" "}
@@ -28,7 +28,7 @@ const About: FunctionalComponent<RoutableProps> = () => {
               </a>{" "}
               channel.
             </p>
-            <article class="pl-5 flex flex-col gap-2 mt-2">
+            <article class="pl-5 flex flex-col gap-2">
               <h4 class="text-purple-300 text-lg">
                 What Your Post Must Include
               </h4>
@@ -58,16 +58,47 @@ const About: FunctionalComponent<RoutableProps> = () => {
                 </li>
               </ul>
             </article>
-            <p class="italic pt-2">
+            <p class="italic">
               It may take some time to verify your addon. You will be notified
               when a decision is made.
             </p>
-            <p class="itlaic pt-2">
+            <p class="itlaic">
               Please only submit addons that you have created.
             </p>
           </article>
-          <h3 class="text-xl pt-2">Custom Data</h3>
-          <p></p>
+          <h3 class="text-xl">Custom Data</h3>
+          <article class="text-slate-400 flex flex-col gap-2">
+            <p>
+              To fix or customize the data scrapped from your addon, you can
+              manually add your own values.
+            </p>
+            <p>
+              To do that, create the file{" "}
+              <p class="px-1 bg-slate-950/50 rounded inline font-medium">
+                meteor-addon-list.json
+              </p>{" "}
+              in the root directory of your addon, and add the fields you wish
+              to overide.
+            </p>
+            <section className="ml-5 p-5 bg-slate-950 border border-purple-300 rounded">
+              <pre className="font-mono text-purple-200 whitespace-pre">
+                {JSON.stringify(
+                  {
+                    description: "A short description of your addon.",
+                    tags: ["PvP", "Utility", "Theme", "..."],
+                    supported_versions: ["1.21.7", "1.21.8"],
+                    icon: "https://meteoraddons.com/icon.png",
+                    discord: "https://discord.gg/XU7Y9G46KD",
+                    homepage: "https://www.meteoraddons.com",
+                  },
+                  null,
+                  2,
+                )}
+              </pre>
+            </section>
+            <h4 class="text-purple-300 text-lg">Supported Tags</h4>
+            <div></div>
+          </article>
         </article>
       </section>
       <section>
